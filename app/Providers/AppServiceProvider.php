e6ca04fc280c1cb008a07e1d6d7da7ed46a5a227
assghard <ivan.dublianski@gmail.com>
2021-02-08 21:38:43 +0100

    todo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if (!\App::runningInConsole()) {
             $ipAddress = (array_key_exists('REMOTE_ADDR', $_SERVER)) ? $_SERVER['REMOTE_ADDR'] : NULL;
-            $technicalBreak = technical_break_config();
+            $technicalBreak = technical_break_config(); // TODO: refactor it: cached config with version???
             if (env('TECHNICAL_BREAK') == true || $technicalBreak['enabled'] == true) {
                 if (empty($ipAddress) || !in_array($ipAddress, $technicalBreak['accessable_ip_addresses'])) { // only admin IPs have access to the website
                     echo view('layouts.technical-break');
